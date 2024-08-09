33cf90b8d461f8ce4b6a1cd324e6b9ab82391dd1
Dilyus29 <garaevdilus1991@mail.ru>
2024-08-09 19:02:32 +0300

    Add files via upload

---
+++ Lesson3.py
@@ -0,0 +1,5 @@
+home_task_count = "12,"
+time = "1.5,"
+cours = "Python,"
+one_task_time = "0.125 часа."
+print('Курс:',cours,'всего задач:',home_task_count, 'затрачено часов:',time,'среднее время выполнения',one_task_time)
